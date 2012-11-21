eec3f77dcce65cfc59207bf5f1a8343edaaded4c
liuwei <liuwei33@gmail.com>
2012-11-21 11:38:18 +0800

    This is just a new computer's greeting!

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 // posts -- data model
 // Loaded on both the client and the server
-
+// My new computer's greeting!
 ///////////////////////////////////////////////////////////////////////////////
 // Posts
 Posts = new Meteor.Collection("posts");
